36009dadb06be0a53b244659d6cb1b1dd0a646ee
Fernando Valdivia <fjvaldiv@calpoly.edu>
2021-01-06 11:16:55 -0800

    fixed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-println("Hello World, this is my 3rd year at Cal Poly.");
-=======
 public class Main {
    public static void main(String[] args) {
       System.out.println("Hello World, this is my 3rd year at Cal Poly.);
    }
 }
->>>>>>> main
